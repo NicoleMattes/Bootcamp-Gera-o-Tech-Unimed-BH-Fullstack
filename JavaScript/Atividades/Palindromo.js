6c07f389848dabac4ab6997bd0165bd297628a99
NicoleMattes <nicolemattesafonso06@gmail.com>
2022-06-28 22:15:46 -0300

    atividade pratica

--- JavaScript/Atividades/Palindromo.js
+++ JavaScript/Atividades/Palindromo.js
@@ -3,23 +3,24 @@
 // function verificarPolidromo(string) {
 //     if(!string) return;
 
-//      return string.split("").reverse().join("") == string;
-// }
+//     return string.split("").reverse().join("") == string;
+//  }
 
-// console.log(verificarPolidromo('omo'));
+//  console.log(verificarPolidromo("Nicole"));
+
 
 /* solução 2*/
 
-
 function verificarPolidromo2(string) {
-    if(!string) return "String inexistente";{
+    if(!string) return "String inexistente";
 
-    for(let i = 0; i < string.length /2; i++);}
-{
-    if (string[1] !== [string.length-1 - i]);
+    for(let i = 0; i < string.length / 2; i++) {
+        if (string[1] !== string[string.length - 1 - i]) {
+            return false;
+        }    
     }
+
+    return true;
 }
 
-// return true;
-
-console.log(verificarPolidromo2('abba'));
+console.log(verificarPolidromo2("abba"));
