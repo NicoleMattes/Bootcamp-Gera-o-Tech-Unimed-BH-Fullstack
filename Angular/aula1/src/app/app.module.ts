3cfe82540adfa2030b18bd89c84c18b4c7b1399a
NicoleMattes <nicolemattesafonso06@gmail.com>
2022-07-05 21:22:00 -0300

    update

--- Angular/aula1/src/app/app.module.ts
+++ Angular/aula1/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list.component';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent
+    CourseListComponent,
+    FirstComponentComponent
   ],
   imports: [
     BrowserModule,
